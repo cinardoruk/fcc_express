afd1e33bdd89cc53a33b61f1d22aa9f0b98fdd9e
Cinar Doruk <cinar.doruk@gmail.com>
2025-07-23 20:22:05 +0300

    express.static middleware

--- myApp.js
+++ myApp.js
@@ -1,12 +1,11 @@
 let express = require('express');
 let app = express();
 
-console.log("Hello World")
-
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/views/index.html');
 });
 
+app.use('/public', express.static())
 
 
 
