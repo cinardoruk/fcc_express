caa7b21415cfcfdda24426de0ff271796389a750
Cinar Doruk <cinar.doruk@gmail.com>
2025-07-24 10:09:58 +0300

    .env file added, read from

--- myApp.js
+++ myApp.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 let express = require('express');
 let app = express();
 
@@ -7,7 +9,16 @@ app.get('/', (req, res) => {
 
 app.get('/json', (req, res) => {
 
-	res.json({"message": "Hello json"});
+	let response;
+	if (process.env.MESSAGE_STYLE === 'uppercase'){
+		response = "HELLO JSON";
+	}
+	else{
+		response = "Hello json";
+	}
+
+	res.json({"message": response});
+
 });
 
 app.use('/public', express.static(__dirname + '/public'))
